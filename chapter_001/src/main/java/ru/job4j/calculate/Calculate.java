d677bdaeef5c26ee0b6bc4e8c61bf98047e12874
DmK78 <dmk78@inbox.ru>
2019-05-14 16:11:46 +0500

    1.3. JavaDoc. Code convention[#141736]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
+/**
+* Calculate.
+*
+* author: Dmitry Kolganov (dmk78@inbox.ru)
+*/
+
 
 public class Calculate {
+
+	/**
+	* main
+	* @param. args - args.
+	*/
 
     public static void main(String[] args) {
         System.out.println("Hello world");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Dmitry Kolganov (dmk78@inbox.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
