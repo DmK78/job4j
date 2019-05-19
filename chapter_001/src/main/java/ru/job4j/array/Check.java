a1fb513058e7deff155d92b61c1770bef1784d4a
DmK78 <dmk78@inbox.ru>
2019-05-19 20:12:08 +0500

    6.3. Массив заполнен true или false[#141713]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+/**
+ * @author Dmitry Kolganov (dmk78@inbox.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+
+/**
+ * Проверяет массив на однородность элементов.
+ */
+public class Check {
+    public boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int i = 0; i < data.length - 1; i++) {
+            if (data[i] != data[i + 1]) {
+                result = false;
+                return result;
+            }
+        }
+        return result;
+    }
+}
