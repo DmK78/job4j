4439f0e84da38edb8e4f34d69522e536f52bf0ea
DmK78 <dmk78@inbox.ru>
2019-05-19 13:04:29 +0500

    5.5. Рефакторинг кода.[#141700]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -5,21 +5,41 @@ package ru.job4j.loop;
  * @since 0.1
  */
 
+import java.util.function.BiPredicate;
+
 /**
  * Программа рисования треугольника.
  */
 public class Paint {
-    /**
-     * Левая часть пирамиды
-     *
-     * @param height - высота.
-     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    private String loopBy(int height, int widht, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int width = 2 * height - 1;
         for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+            for (int column = 0; column != widht; column++) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
