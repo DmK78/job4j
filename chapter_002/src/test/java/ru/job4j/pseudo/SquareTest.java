f24abb43016c1f7c06b841d660b431f3112e892d
DmK78 <dmk78@inbox.ru>
2019-05-28 13:38:08 +0500

    5. Рефакторинг теста @Before @After[#141785]

--- chapter_002/src/test/java/ru/job4j/pseudo/SquareTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/SquareTest.java
@@ -1,9 +1,14 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.job4j.ru.job4j.pseudo.Square;
 import ru.job4j.ru.job4j.pseudo.Triangle;
 import ru.job4j.tracker.Tracker;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -15,8 +20,26 @@ import static org.junit.Assert.assertThat;
  * @since 0.1
  */
 public class SquareTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
+
         Square square = new Square();
         assertThat(
                 square.draw(),
@@ -29,10 +52,12 @@ public class SquareTest {
                                 .toString()
                 )
         );
+
     }
 
     @Test
-    public void whenDrawTriamgle() {
+    public void whenDrawTriangle() {
+
         Triangle triangle = new Triangle();
         assertThat(
                 triangle.draw(),
@@ -45,6 +70,7 @@ public class SquareTest {
                                 .toString()
                 )
         );
+
     }
 
 
