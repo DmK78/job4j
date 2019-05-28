c0ae6312eebe8a4fee9849b0b44d1ebfa90fd097
DmK78 <dmk78@inbox.ru>
2019-05-28 12:02:13 +0500

    4. Используя шаблон проектирования - стратегию[#141790]

---
+++ chapter_002/src/test/java/ru/job4j/pseudo/SquareTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.pseudo;
+
+import org.junit.Test;
+import ru.job4j.ru.job4j.pseudo.Square;
+import ru.job4j.ru.job4j.pseudo.Triangle;
+import ru.job4j.tracker.Tracker;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * @author Dmitry Kolganov (dmk78@inbox.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SquareTest {
+    @Test
+    public void whenDrawSquare() {
+        Square square = new Square();
+        assertThat(
+                square.draw(),
+                is(
+                        new StringBuilder()
+                                .append("++++++\n")
+                                .append("++++++\n")
+                                .append("++++++\n")
+                                .append("++++++\n")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenDrawTriamgle() {
+        Triangle triangle = new Triangle();
+        assertThat(
+                triangle.draw(),
+                is(
+                        new StringBuilder()
+                                .append("   +   \n")
+                                .append("  +++  \n")
+                                .append(" +++++ \n")
+                                .append("+++++++\n")
+                                .toString()
+                )
+        );
+    }
+
+
+}
