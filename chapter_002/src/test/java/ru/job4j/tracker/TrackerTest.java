fd927eaae0e117eaf94ff92199518782a45b17f1
DmK78 <dmk78@inbox.ru>
2019-05-30 10:37:29 +0500

    начинаю переделывать StartUITest

--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -10,7 +10,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
-    private final Tracker tracker = new Tracker();
+
 
     @Before
 
@@ -20,6 +20,7 @@ public class TrackerTest {
 
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
         long created = System.currentTimeMillis();
         Item item = new Item("test1", "testDescription", created);
         tracker.add(item);
@@ -29,6 +30,7 @@ public class TrackerTest {
 
     @Test
     public void whenReplaceNameThenReturnNewName() {
+        Tracker tracker = new Tracker();
         Item previous = new Item("test1", "testDescription", 123L);
         // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
         tracker.add(previous);
@@ -44,6 +46,7 @@ public class TrackerTest {
 
     @Test
     public void whenDeleteItemThenReturnNewArray() {
+        Tracker tracker = new Tracker();
         Item first = new Item("test1", "testDescription", 123L);
         Item second = new Item("test2", "testDescription", 223L);
         Item third = new Item("test3", "testDescription", 323L);
@@ -56,11 +59,12 @@ public class TrackerTest {
         result = tracker.findById(third.getId());
         assertThat(result.getName(), is(third.getName()));
         int size = tracker.getPosition();
-        assertThat(tracker.getPosition(), is(2));
+        assertThat(size, is(2));
     }
 
     @Test
     public void whenFindAllItemThenShowThree() {
+        Tracker tracker = new Tracker();
         Item first = new Item("test1", "testDescription", 123L);
         Item second = new Item("test2", "testDescription", 223L);
         Item third = new Item("test3", "testDescription", 323L);
@@ -73,6 +77,7 @@ public class TrackerTest {
 
     @Test
     public void whenFindByNameThenShowTwoItems() {
+        Tracker tracker = new Tracker();
         Item first = new Item("test1", "testDescription", 123L);
         Item second = new Item("test2", "testDescription", 223L);
         Item third = new Item("test1", "testDescription", 323L);
