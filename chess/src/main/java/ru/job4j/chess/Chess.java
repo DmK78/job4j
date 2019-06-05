e073e08cb7421bd3d169cf555d2619a7841c6dda
DmK78 <dmk78@inbox.ru>
2019-06-05 17:23:36 +0500

    Каркас шахматной доски[#141822] - реализация ходов слона

--- chess/src/main/java/ru/job4j/chess/Chess.java
+++ chess/src/main/java/ru/job4j/chess/Chess.java
@@ -61,10 +61,11 @@ public class Chess extends Application {
         );
         rect.setOnMouseReleased(
                 event -> {
-                    if (board.move(this.findBy(momento.getX(), momento.getY()), this.findBy(event.getX(), event.getY()))) {
+                    try {
+                        board.move(this.findBy(momento.getX(), momento.getY()), this.findBy(event.getX(), event.getY()));
                         rect.setX(((int) event.getX() / 40) * 40 + 5);
                         rect.setY(((int) event.getY() / 40) * 40 + 5);
-                    } else {
+                    } catch (FigureNotFoundException | ImposibleMoveException | OccupiedWayException e) {
                         rect.setX(((int) momento.getX() / 40) * 40 + 5);
                         rect.setY(((int) momento.getY() / 40) * 40 + 5);
                     }
