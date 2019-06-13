bcb8dd33c5318e04492db8b63f1abecb5b39fa00
DmK78 <dmk78@inbox.ru>
2019-06-13 09:41:31 +0500

    Банкомат по размену купюр

--- chapter_002/src/main/java/ru/job4j/cashmachine/CashMachine.java
+++ chapter_002/src/main/java/ru/job4j/cashmachine/CashMachine.java
@@ -4,14 +4,15 @@ package ru.job4j.cashmachine;
  * @version $Id$
  * @since 0.1
  */
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 /**
- * Автомат размена купюр
+ * TODO: comment
+ *
+ * @author job4j
+ * @since 28.07.2016
  */
 public class CashMachine {
 
@@ -22,63 +23,24 @@ public class CashMachine {
     }
 
     public List<List<Integer>> exchange(int note) {
-        List<List<Integer>> result = new ArrayList<>();
-        int remain = note;
-        for (int currentCoin : values) {
-            List<Integer> currentArr = new ArrayList<>();
-            if (note < currentCoin) {
-                continue;
-            }
-            if (note % currentCoin == 0) {
-                for (int i = 0; i < note / currentCoin; i++) {
-                    currentArr.add(currentCoin);
-                    remain -= currentCoin;
-                }
-                result.add(currentArr);
-                if (note / currentCoin > 1) {
-                    remain = note;
-                    currentArr = new ArrayList<>();
-                    currentArr.add(currentCoin);
-                    remain -= currentCoin;
-                    for (int anotherCoin : values) {
-                        while (remain > 0) {
-                            if (anotherCoin < currentCoin && anotherCoin <= remain) {
-                                currentArr.add(anotherCoin);
-                                remain -= anotherCoin;
-                            } else {
-                                break;
-                            }
-                        }
-                    }
-                    currentArr.sort(null);
-                    int sum = 0;
-                    for (int i : currentArr) {
-                        sum += i;
-                    }
-                    if (sum == note) {
-                        result.add(currentArr);
-                    }
-                }
-            }
-            while (remain > 0) {
-                if (currentCoin <= remain) {
-                    currentArr.add(currentCoin);
-                    remain -= currentCoin;
+        return this.exchange(note, 0);
+    }
 
-                } else {
-                    for (int anotherCoin : values) {
-                        if (anotherCoin < currentCoin && anotherCoin <= remain) {
-                            currentArr.add(anotherCoin);
-                            remain -= anotherCoin;
-                        }
-                    }
+    public List<List<Integer>> exchange(int note, int step) {
+        List<List<Integer>> data = new ArrayList<>();
+        for (int index = step; index != this.values.length; index++) {
+            final int value = this.values[index];
+            int rsl = note - this.values[index];
+            if (rsl == 0) {
+                data.add(new ArrayList<>(Collections.singletonList(value)));
+            } else {
+                for (List<Integer> sub : this.exchange(rsl, index)) {
+                    sub.add(value);
+                    data.add(sub);
                 }
-            }
-            if (note % currentCoin != 0) {
-                remain = note;
-                result.add(currentArr);
             }
         }
-        return result;
+        return data;
     }
+
 }
