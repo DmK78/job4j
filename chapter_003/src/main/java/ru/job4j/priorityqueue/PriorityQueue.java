a79f65c0ec9e5fed9dc4f374b1c29e686671b27c
DmK78 <dmk78@inbox.ru>
2019-06-20 16:58:43 +0500

    2. Очередь с приоритетом на LinkedList[#141761]-исправлено

--- chapter_003/src/main/java/ru/job4j/priorityqueue/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/priorityqueue/PriorityQueue.java
@@ -1,6 +1,8 @@
 package ru.job4j.priorityqueue;
 
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class PriorityQueue {
     private LinkedList<Task> tasks = new LinkedList<>();
@@ -13,18 +15,33 @@ public class PriorityQueue {
      * @param task задача
      */
     public void put(Task task) {
-        int index = 0;
-        for (int i = 0; i < tasks.size(); i++) {
-            if (tasks.get(i).getPriority() > task.getPriority()) {
-                index = i;
+        int index = -1;
+        Iterator<Task> it = tasks.listIterator();
+        while (it.hasNext()) {
+            Task cutTask = it.next();
+            if (cutTask.getPriority() > task.getPriority() && tasks.size() != 0) {
+                index = tasks.indexOf(cutTask);
                 break;
             }
-            index = i + 1;
         }
-        tasks.add(index, task);
+        if (index != -1) {
+            tasks.add(index, task);
+        } else {
+            tasks.add(task);
+        }
     }
 
     public Task take() {
         return this.tasks.poll();
     }
+
+    public static void main(String[] args) {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("3", 3));
+        queue.put(new Task("2", 2));
+        queue.put(new Task("1", 1));
+
+
+    }
 }
+
