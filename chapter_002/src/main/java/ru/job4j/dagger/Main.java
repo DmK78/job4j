fc37188debbc0b44d528bc6fea2d8356b753fbc1
DmK78 <dmk78@inbox.ru>
2019-10-15 09:56:28 +0500

    1. Инъекции зависимостей. Dagger. [#186900]

---
+++ chapter_002/src/main/java/ru/job4j/dagger/Main.java
@@ -0,0 +1,9 @@
+package ru.job4j.dagger;
+
+public class Main
+{
+    public static void main(String[] args) {
+        InputComponent component = DaggerInputComponent.create();
+        component.instance().add();
+    }
+}
