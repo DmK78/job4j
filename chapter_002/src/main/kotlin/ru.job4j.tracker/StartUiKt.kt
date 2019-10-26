4a4ebdb46cebe864e4a694489122621e8dcd0f67
DmK78 <dmk78@inbox.ru>
2019-10-26 22:53:20 +0500

    4. Класс-объект[#191451]- добавил методы

--- chapter_002/src/main/kotlin/ru.job4j.tracker/StartUiKt.kt
+++ chapter_002/src/main/kotlin/ru.job4j.tracker/StartUiKt.kt
@@ -2,30 +2,34 @@ package ru.job4j.tracker
 
 import java.util.ArrayList
 
-object StartUiKt {
-    fun addItem(item: Item){
-        println("Добавление заявки")
-    }
-    fun showAll(){
-        println("Печать всех заявок")
-    }
+class StartUiKt private constructor() {
+    companion object {
+        fun addItem(item: Item) {
+            println("Добавление заявки")
+        }
 
-    fun init(){
-        println("0 - Добавить заявку")
-        println("1 - Показать все заявки")
-        println("6 - Выйти")
-        do {var i = readLine()!!.toInt()
-            when(i){
-                0 -> addItem(Item(name="1",desc = "1"))
-                1 -> showAll()
+        fun showAll() {
+            println("Печать всех заявок")
+        }
+
+        fun init() {
+            println("0 - Добавить заявку")
+            println("1 - Показать все заявки")
+            println("6 - Выйти")
+            do {
+                var i = readLine()!!.toInt()
+                when (i) {
+                    0 -> addItem(Item(name = "1", desc = "1"))
+                    1 -> showAll()
 
 
-            }
+                }
 
-        }while (i!=6)
+            } while (i != 6)
+        }
     }
 }
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     StartUiKt.init()
 }
